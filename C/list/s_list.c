ba11daefbbebc6eec512ac7046cdcc6ca5a9d645
Pineapple <pineapple_cpp@163.com>
2021-09-02 11:05:11 +0800

    update s_list

--- C/list/s_list.c
+++ C/list/s_list.c
@@ -1,127 +1,161 @@
 // s_list.c - 2021年 八月 30日
-// 测试单链表
+// 实现单链表
 
+#include <string.h>
 #include "s_list.h"
 
-void show_books(Item *pi);
+static Node *find_pre(List list, Node *pn);
 
-char get_choice(void);
-
-char get_first(void);
-
-void eat_line(void);
-
-void add_book(List *pl);
-
-char *s_gets(char *string, int len);
-
-int main(void)
+/**
+ * 初始化链表为空
+ * @param pl 链表指针
+ */
+inline void list_init(List *pl)
 {
-#ifdef WIN32 // 如果是Windows系统，则修改控制台的编码为 UTF-8
-	system("chcp 65001 > nul");
-#endif
-	List books;
-	int choice;
+	*pl = NULL;
+}
 
-	// 初始化链表
-	list_init(&books);
+/**
+ * 判断链表是否为空
+ * @param list 链表
+ * @return
+ */
+inline int list_is_empty(List list)
+{
+	if (list == NULL)
+		return 1;
+	return 0;
+}
 
-	printf("======欢迎来到图书管理系统！======\n");
+/**
+ * 判断链表是否已满
+ * @param plist 链表指针
+ * @return
+ */
+inline int list_is_full()
+{
+	Node *pn = (Node *)malloc(sizeof(Node));
 
-	while ((choice = get_choice()) != 'q') {
-		switch (choice) {
-		case 'a': // 添加书籍
-			add_book(&books);
-			break;
-		case 'b': // 查看书籍
-			list_for_each (books, show_books)
-				;
-			break;
-		case 'c': // 书记个数
-			printf("书籍个数：%d\n", list_count(books));
-			break;
-		case 'd': // 清空书籍
-			list_clean(&books);
-			break;
-		default:
-			fprintf(stderr, "程序错误！\n");
-			break;
-		}
-	}
-	printf("再见！\n");
+	if (pn == NULL)
+		return 1;
+	free(pn);
 
 	return 0;
 }
 
-void show_books(Item *pi)
+/**
+ * 计算链表内节点个数
+ * @param list 链表
+ * @return
+ */
+unsigned int list_count(List list)
 {
-	printf("title: %s\nauthor: %s\nprice: %.2f", pi->title, pi->author,
-	       pi->price);
-	printf("\n------------------------------\n");
-}
+	unsigned int count = 0;
 
-char get_choice(void)
-{
-	int ch;
-
-	printf("输入您的选择：\n");
-	printf("a. 添加书籍		b. 查看书籍\n");
-	printf("c. 书籍个数		d. 清空书籍\n");
-	printf("q. 退出程序\n");
-	ch = get_first();
-	while ((ch < 'a' || ch > 'd') && ch != 'q') {
-		printf("输入错误，请重新输入：\n");
-		ch = get_first();
+	Node *pn = list;
+	while (pn != NULL) {
+		pn = pn->next;
+		count++;
 	}
 
-	return ch;
+	return count;
 }
 
-char get_first(void)
+/**
+ * 向链表末尾添加一个节点
+ * @param pl 链表指针
+ * @param pi 节点内数据的指针
+ */
+void list_append(List *pl, Item *pi)
 {
-	int ch;
-
-	ch = getchar();
-	eat_line();
-
-	return ch;
-}
-
-inline void eat_line(void)
-{
-	while (getchar() != '\n')
-		continue;
-}
-
-void add_book(List *pl)
-{
-	Item item;
-
-	printf("书名：");
-	s_gets(item.title, MAX_TITLE);
-	printf("作者：");
-	s_gets(item.author, MAX_AUTHOR);
-	printf("价格：");
-	scanf("%lf", &item.price);
-	eat_line();
-
-	list_append(pl, &item);
-}
-
-char *s_gets(char *string, int len)
-{
-	char *ret_val;
-	int i = 0;
-
-	ret_val = fgets(string, len, stdin);
-	if (ret_val) { // ret_val != NULL
-		while (string[i] != '\0' && string[i] != '\n')
-			i++;
-		if (string[i] == '\n')
-			string[i] = '\0';
-		else
-			eat_line();
+	// 1 创建一个节点并分配内存（判断内存是否已满）
+	Node *pn_new = (Node *)malloc(sizeof(Node));
+	if (pn_new == NULL) {
+		fprintf(stderr, "分配内存失败！内存不足！\n");
+		return;
 	}
 
-	return ret_val;
+	// 2 将数据放入节点
+	pn_new->item = *pi;
+	pn_new->next = NULL;
+
+	// 3 判断是否为空节点，若是，则直接赋值；如不是，则遍历到最后一个节点再连接
+	Node *pn = *pl;
+	if (pn == NULL)
+		*pl = pn_new;
+	else {
+		while (pn->next != NULL)
+			pn = pn->next;
+		pn->next = pn_new;
+	}
+	fprintf(stdout, "添加节点成功！\n");
+}
+
+/**
+ * 删除一个节点
+ * @param pl 链表指针
+ * @param pi 指向待删除的节点元素
+ */
+void list_delete(List *pl, Item *pi)
+{
+	Node *pn = *pl;
+
+	if (&pn->item == pi) {
+		free(*pl);
+		list_init(pl); // 表头被free掉，需要重新指向NULL，即初始化
+	} else {
+		while (&pn->item != pi)
+			pn = pn->next;
+		Node *pn_pre = find_pre(*pl, pn);
+		pn_pre->next = pn->next;
+		free(pn);
+	}
+	printf("删除节点成功！");
+}
+
+/**
+ * 传递给定函数遍历链表
+ * @param list 链表
+ * @param pf 函数指针
+ */
+void list_for_each (List list, void(*pf)(Item *))
+{
+	Node *pn = list;
+
+	while (pn != NULL) {
+		(*pf)(&pn->item); // 执行函数
+		pn = pn->next;
+	}
+}
+
+/**
+ * 清空链表
+ * @param pl 链表指针
+ */
+void list_clean(List *pl)
+{
+	Node *pn_save;
+
+	while (*pl != NULL) {
+		pn_save = (*pl)->next; // 保存下一节点的地址
+		free(*pl);
+		*pl = pn_save;
+	}
+	printf("清空链表成功！\n");
+}
+
+/**
+ * 找到给定节点的上一个节点
+ * @param list 链表
+ * @param pn 结点指针
+ * @return
+ */
+static Node *find_pre(List list, Node *pn)
+{
+	Node *pn_pre = list;
+
+	while (pn_pre->next != pn)
+		pn_pre = pn->next;
+
+	return pn_pre;
 }
--- C/list/s_list.h
+++ C/list/s_list.h
@@ -31,120 +31,20 @@ typedef struct node {
 
 typedef Node *List;
 
-/**
- * 初始化链表为空
- * @param pl 链表指针
- */
-static inline void list_init(List *pl)
-{
-	*pl = NULL;
-}
+void list_init(List *pl);
 
-/**
- * 判断链表是否为空
- * @param list 链表
- * @return
- */
-static inline int list_is_empty(List list)
-{
-	if (list == NULL)
-		return 1;
-	return 0;
-}
+int list_is_empty(List list);
 
-/**
- * 判断链表是否已满
- * @param plist 链表指针
- * @return
- */
-static inline int list_is_full()
-{
-	Node *pn = (Node *)malloc(sizeof(Node));
+int list_is_full();
 
-	if (pn == NULL)
-		return 1;
-	free(pn);
+unsigned int list_count(List list);
 
-	return 0;
-}
+void list_append(List *pl, Item *pi);
 
-/**
- * 计算链表内节点个数
- * @param list 链表
- * @return
- */
-unsigned int list_count(List list)
-{
-	unsigned int count = 0;
+void list_delete(List *pl, Item *pi);
 
-	Node *pn = list;
-	while (pn != NULL) {
-		pn = pn->next;
-		count++;
-	}
+void list_for_each (List list, void(*pf)(Item *));
 
-	return count;
-}
-
-/**
- * 向链表末尾添加一个节点
- * @param pl 链表指针
- * @param pi 节点内数据的指针
- */
-void list_append(List *pl, Item *pi)
-{
-	// 1 创建一个节点并分配内存（判断内存是否已满）
-	Node *pn_new = (Node *)malloc(sizeof(Node));
-	if (pn_new == NULL) {
-		fprintf(stderr, "分配内存失败！内存不足！\n");
-		return;
-	}
-
-	// 2 将数据放入节点
-	pn_new->item = *pi;
-	pn_new->next = NULL;
-
-	// 3 判断是否为空节点，若是，则直接赋值；如不是，则遍历到最后一个节点再连接
-	Node *pn = *pl;
-	if (pn == NULL)
-		*pl = pn_new;
-	else {
-		while (pn->next != NULL)
-			pn = pn->next;
-		pn->next = pn_new;
-	}
-	fprintf(stdout, "添加节点成功！\n");
-}
-
-/**
- * 传递给定函数遍历链表
- * @param list 链表
- * @param pf 函数指针
- */
-void list_for_each (List list, void(*pf)(Item *))
-{
-	Node *pn = list;
-
-	while (pn != NULL) {
-		(*pf)(&pn->item); // 执行函数
-		pn = pn->next;
-	}
-}
-
-/**
- * 清空链表
- * @param pl 链表指针
- */
-void list_clean(List *pl)
-{
-	Node *pn_save;
-
-	while (*pl != NULL) {
-		pn_save = (*pl)->next; // 保存下一节点的地址
-		free(*pl);
-		*pl = pn_save;
-	}
-	printf("清空链表成功！\n");
-}
+void list_clean(List *pl);
 
 #endif // S_LIST_H_
