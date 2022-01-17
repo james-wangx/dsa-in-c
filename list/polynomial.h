0331d8e9fd4dacf407cbeac59571c7cf1a8d205b
Pineapple <pineapple_cpp@163.com>
2022-01-17 10:53:59 +0800

    更新 单链表一元多项式

--- list/polynomial.h
+++ list/polynomial.h
@@ -1,6 +1,6 @@
 /**
  * @file polynomial.h
- * @date 2021-11-24
+ * @date 2022-01-17
  * @author Pineapple (pineapple_cpp@163.com)
  * 
  * @brief 一元多项式 ADT
@@ -27,7 +27,6 @@ typedef PtrToNode Position;
 
 void ListInit(List list);
 _Bool ListIsEmpty(List list);
-_Bool PosIsLast(Position pos);
 Position ListLast(List list);
 Position ListFind(List list, Pitem item);
 Position ListFindPrev(List list, Pitem item);
@@ -43,4 +42,4 @@ List PolyMin(List list1, List list2, List new);
 List PolyMul(List list1, List list2, List new);
 void PolyMerge(List list);
 
-#endif // _DSAA_POLYNOMIAL_H
+#endif // _POLYNOMIAL_H
